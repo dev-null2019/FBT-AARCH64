2ab69bea8925714ea50fe94a386905662084b48a
dev-null2019 <56407056+dev-null2019@users.noreply.github.com>
2024-02-22 12:32:42 +0100

    updated gcc version

--- setup.sh
+++ setup.sh
@@ -93,7 +93,7 @@ then
 	echo "#########################################"
 	sudo apt -y install python3 python3-dev python3-pip nanopb protobuf-compiler
  	echo "###################################################"
-	echo "##### Create Python venv and activate it#####"
+	echo "##### Create Python venv and activate it ##########"
 	echo "###################################################"
  	python3 -m venv ./venv
   	source ./venv/bin/activate
@@ -104,49 +104,49 @@ then
 	pip install nanopb==0.4.7 protobuf grpcio-tools heatshrink2==0.12.0 scons==4.5.2 ansi==0.3.6 colorlog==6.7.0 pillow==9.4.0
 	pip install pyelftools
  	echo "########################################################################################"
-	echo "##### Downloading GCC Toolchain specific for FlipperZero Firmware as of April 2023 #####"
+	echo "##### Downloading GCC Toolchain specific for FlipperZero Firmware as of February 2024 #####"
 	echo "########################################################################################"
-	wget -P $HOME/Downloads/ "https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack/releases/download/v10.3.1-2.1/xpack-arm-none-eabi-gcc-10.3.1-2.1-linux-arm64.tar.gz"
+	wget -P $HOME/Downloads/ "https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack/releases/download/v12.3.1-1.2/xpack-arm-none-eabi-gcc-12.3.1-1.2-linux-arm64.tar.gz"
 	echo "###############################"
 	echo "##### Extracting tar file #####"
 	echo "###############################"
-	sudo tar xf $HOME/Downloads/xpack-arm-none-eabi-gcc-10.3.1-2.1-linux-arm64.tar.gz -C /usr/share/
+	sudo tar xf $HOME/Downloads/xpack-arm-none-eabi-gcc-12.3.1-1.2-linux-arm64.tar.gz -C /usr/share/
 	echo "############################################"
 	echo "##### Extracting done, adding symlinks #####"
 	echo "############################################"
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-addr2line /usr/bin/arm-none-eabi-addr2line
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-ar /usr/bin/arm-none-eabi-ar
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-as /usr/bin/arm-none-eabi-as
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-c++ /usr/bin/arm-none-eabi-c++
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-c++filt /usr/bin/arm-none-eabi-c++filt
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-cpp /usr/bin/arm-none-eabi-cpp
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-elfedit /usr/bin/arm-none-eabi-elfedit
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcc-10.3.1 /usr/bin/arm-none-eabi-gcc-10.3.1
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcc-ar /usr/bin/arm-none-eabi-gcc-ar
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcc-nm /usr/bin/arm-none-eabi-gcc-nm
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcc-ranlib /usr/bin/arm-none-eabi-gcc-ranlib
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcov /usr/bin/arm-none-eabi-gcov
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcov-dump /usr/bin/arm-none-eabi-gcov-dump
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gcov-tool /usr/bin/arm-none-eabi-gcov-tool
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gdb-add-index /usr/bin/arm-none-eabi-gdb-add-index
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gdb-add-index-py3 /usr/bin/arm-none-eabi-gdb-add-index-py3
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gdb-add-index-py3 /usr/bin/arm-none-eabi-gdb-add-index-py
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gdb-py3 /usr/bin/arm-none-eabi-gdb-py3
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-gprof /usr/bin/arm-none-eabi-gprof
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-ld /usr/bin/arm-none-eabi-ld
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-ld.bfd /usr/bin/arm-none-eabi-ld.bfd
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-lto-dump /usr/bin/arm-none-eabi-lto-dump
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-nm /usr/bin/arm-none-eabi-nm
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-ranlib /usr/bin/arm-none-eabi-ranlib
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-readelf /usr/bin/arm-none-eabi-readelf
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-strings /usr/bin/arm-none-eabi-strings
-	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-10.3.1-2.1/bin/arm-none-eabi-strip /usr/bin/arm-none-eabi-strip
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-addr2line /usr/bin/arm-none-eabi-addr2line
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-ar /usr/bin/arm-none-eabi-ar
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-as /usr/bin/arm-none-eabi-as
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-c++ /usr/bin/arm-none-eabi-c++
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-c++filt /usr/bin/arm-none-eabi-c++filt
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-cpp /usr/bin/arm-none-eabi-cpp
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-elfedit /usr/bin/arm-none-eabi-elfedit
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcc-10.3.1 /usr/bin/arm-none-eabi-gcc-10.3.1
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcc-ar /usr/bin/arm-none-eabi-gcc-ar
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcc-nm /usr/bin/arm-none-eabi-gcc-nm
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcc-ranlib /usr/bin/arm-none-eabi-gcc-ranlib
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcov /usr/bin/arm-none-eabi-gcov
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcov-dump /usr/bin/arm-none-eabi-gcov-dump
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gcov-tool /usr/bin/arm-none-eabi-gcov-tool
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gdb-add-index /usr/bin/arm-none-eabi-gdb-add-index
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gdb-add-index-py3 /usr/bin/arm-none-eabi-gdb-add-index-py3
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gdb-add-index-py3 /usr/bin/arm-none-eabi-gdb-add-index-py
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gdb-py3 /usr/bin/arm-none-eabi-gdb-py3
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-gprof /usr/bin/arm-none-eabi-gprof
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-ld /usr/bin/arm-none-eabi-ld
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-ld.bfd /usr/bin/arm-none-eabi-ld.bfd
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-lto-dump /usr/bin/arm-none-eabi-lto-dump
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-nm /usr/bin/arm-none-eabi-nm
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-ranlib /usr/bin/arm-none-eabi-ranlib
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-readelf /usr/bin/arm-none-eabi-readelf
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-strings /usr/bin/arm-none-eabi-strings
+	sudo ln -s /usr/share/xpack-arm-none-eabi-gcc-12.3.1-1.2/bin/arm-none-eabi-strip /usr/bin/arm-none-eabi-strip
  	deactivate
 	echo "#########################################################################"
 	echo "##### Done. Clone FlipperZero Firmware, activate the virtual environment and Run FBT with FBT_NOENV=1 #####"
